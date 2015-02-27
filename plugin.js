c92f7613548082f4c9fa6a0763c49d2e6fe85327
Moritz Schmidt <fusselwurm@gmail.com>
2015-02-27 22:06:11 +0100

    fix sorting

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 var
     _ = require('underscore'),
     titleToTimestamp = _.memoize(function (title) {
-        var matches = title.match(/([0-9]{4}-[0-9]{2}-[0-9]{2})\s/);
+        var matches = title.match(/([0-9]{4}-[0-9]{2}-[0-9]{2})[^a-z0-9]/i);
         if (!matches) {
             return 0;
         }
@@ -15,6 +15,11 @@ module.exports.colorTopic = function (data, cb) {
     });
 
     data.category.topics = data.category.topics.sort(function (a, b) {
+        if (b.pinned && !a.pinned) {
+            return 1;
+        } else if (!b.pinned && a.pinned) {
+            return -1;
+        }
         return b.referencetime - a.referencetime;
     });
 
